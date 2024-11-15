5da1466852b36816a1e8882349f361b9fc6f4516
Fangkang <fangkangmi@outlook.com>
2024-11-15 16:17:42 +0000

    Add data type and compound types examples to variables_mutability module

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,8 @@ fn main(){
     variables_mutability::variable_and_constant();
 
     variables_mutability::shadowing_example();
+
+    variables_mutability::data_type();
+
+    variables_mutability::compound_types();
 }
--- src/variables_mutability.rs
+++ src/variables_mutability.rs
@@ -38,3 +38,66 @@ pub fn shadowing_example() {
 }
 //Rust is ahead of its time in terms of safety and performance.
 //Ahead of its time is a phrase that means that something is more advanced than its contemporaries.
+
+pub fn data_type(){
+    //Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it.
+    //Scalar types represent a single value. Rust has four primary scalar types: integers, floating-point numbers, Booleans, and characters.
+    //Integers are a number without a fractional component. Rust has signed and unsigned integers. Signed integers can be positive, negative, or zero. Unsigned integers can only be positive or zero.
+    //Signed integers are i8, i16, i32, i64, i128, and isize. The number after i indicates the number of bits the integer type takes up.
+    //Unsigned integers are u8, u16, u32, u64, u128, and usize. The number after u indicates the number of bits the integer type takes up.
+    //Integer literals can be expressed in decimal, hexadecimal, octal, binary, and byte (u8 only) formats.
+    //Floating-point numbers are numbers with a fractional component. Rust has two primitive types for floating-point numbers: f32 and f64. The default type is f64 because on modern CPUs it's roughly the same speed as f32 but is capable of more precision.
+    //Boolean types are either true or false. Booleans are one byte in size.
+    //Character types are specified with single quotes, as opposed to strings, which use double quotes. Rust's char type is four bytes in size and represents a Unicode Scalar Value, which means it can represent more than just ASCII.
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {}", guess);
+
+    let x = 2.0; // f64
+    let y: f32 = 3.0; // f32
+
+    let t = true;
+    let f: bool = false;
+
+    let c = 'z';
+    let z = 'ℤ';
+    let heart_eyed_cat = '😻';
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of t is: {}", t);
+    println!("The value of f is: {}", f);
+    println!("The value of c is: {}", c);
+    println!("The value of z is: {}", z);
+    println!("The value of heart_eyed_cat is: {}", heart_eyed_cat);
+
+    //isize and usize depend on the kind of computer your program is running on: 64 bits if you're on a 64-bit architecture and 32 bits if you're on a 32-bit architecture.
+    //The primary situation in which you'd use isize or usize is when indexing some sort of collection.
+    let x: isize = 10;
+    let y: usize = 10;
+}
+
+pub fn compound_types(){
+    //tuple: a general way of grouping together a number of values with a variety of types into one compound type. Tuples have a fixed length: once declared, they cannot grow or shrink in size.
+    //Tuples are useful for functions that need to return more than one value.
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of tup is: {}, {}, {}", x, y, z);
+    println!("The value of tup is: {}, {}, {}", tup.0, tup.1, tup.2);
+
+    //array: every element of an array must have the same type. Arrays in Rust have a fixed length, like tuples.
+    //Arrays are useful when you want your data allocated on the stack rather than the heap.
+
+    //The stack stores values in the order it gets them and removes the values in the opposite order. This is referred to as last in, first out.
+    //All data stored on the stack must have a known, fixed size.
+
+    //the heap is less organized: when you put data on the heap, you request a certain amount of space. 
+    //The operating system finds an empty spot in the heap that is big enough, marks it as being in use, and returns a pointer, which is the address of that location in memory.
+
+    let month: [&str; 12] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    let first = month[0];
+    let second = month[1];
+
+    println!("The first two months are: {}, {}", first, second);
+
+}
